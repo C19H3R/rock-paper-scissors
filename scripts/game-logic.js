f13241e93e7b75533ead736dacbc7dad30e3888a
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-09-12 20:48:19 +0530

    aded ui to the game

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -35,22 +35,51 @@ const singleRound = (playerSelection, computerSelection) => {
     if (computer == "rock" && player == "paper" ||
         computer == "paper" && player == "scissor" ||
         computer == "scissor" && player == "rock")
-        return winnerString("Player", "Computer", player, computer);
+        return winnerString("player", "computer", player, computer);
 
 }
 
 const winnerString = (winner, loser, winnerHand, loserHand) => {
+    const result = `${winner.toUpperCase()} wins , as ${winnerHand.toUpperCase()} beats ${loserHand.toUpperCase()}`;
 
-    return `${winner.toUpperCase()} wins , as ${winnerHand.toUpperCase()} beats ${loserHand.toUpperCase()}`;
-
+    const resultTag =document.getElementById("result");
+    resultTag.textContent=result;
+    updateScore(winner);
 }
 
-const game = () => {
-    var roundCounter = 0;
-    const noOfRounds = 5;
-    while (roundCounter < noOfRounds) {
-        console.log(singleRound(prompt(), computerPlay()));
-        roundCounter++;
+const updateScore = (winner)=>{
+    if(winner=="player"){
+        playerScore++;
+    }else{
+        computerScore++;
     }
+    playerScoreDiv.textContent=playerScore;
+    console.log("player "+playerScore);
+    console.log("computer "+computerScore);
 
+    computerScoreDiv.textContent=computerScore;
+
+    if(playerScore>5||computerScore>5){
+        playerScore=0;
+        computerScore=0;
+        const resultTag =document.getElementById("result");
+        resultTag.textContent=`${winner} wins the full match`;
+    }
 }
+
+let playerScore=0;
+let computerScore=0;
+const playerScoreDiv=document.getElementById("player-score");
+const computerScoreDiv=document.getElementById("computer-score");
+
+
+
+
+const rockBtn=document.getElementById("rock");
+const paperBtn=document.getElementById("paper");
+const scissorBtn=document.getElementById("scissors");
+
+rockBtn.addEventListener("click",(e)=>(singleRound("rock",computerPlay())));
+paperBtn.addEventListener("click",(e)=>(singleRound("paper",computerPlay())));
+scissorBtn.addEventListener("click",(e)=>(singleRound("scissor",computerPlay())));
+
